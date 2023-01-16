5180859a8a89f0b27031a0cde71c0ee0cfe62e51
Anatolii Petrov <hello@aswitch.me>
2023-01-16 22:15:27 +0100

    Initial Commit

---
+++ RPS Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RPS Game
+//
+//  Created by Anatoliy Petrov on 16.1.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RPS Game/RPS_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_GameApp.swift
+//  RPS Game
+//
+//  Created by Anatoliy Petrov on 16.1.23..
+//
+
+import SwiftUI
+
+@main
+struct RPS_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
